ad89ac14b265c95e50864ae850857914e71d85a2
Petrq95 <44844331+Petrq95@users.noreply.github.com>
2018-11-27 21:44:20 +0200

    Add files via upload

---
+++ Calc/js/index.js
@@ -0,0 +1,48 @@
+function mumOne(){
+	calc.input.value += '1'
+}
+function mumTwo(){
+	calc.input.value += '2'
+}
+function mumThree(){
+	calc.input.value += '3'
+}
+function mumFour(){
+	calc.input.value += '4'
+}
+function mumFive(){
+	calc.input.value += '5'
+}
+function mumSix(){
+	calc.input.value += '6'
+}
+function mumSeven(){
+	calc.input.value += '7'
+}
+function mumEight(){
+	calc.input.value += '8'
+}
+function mumNine(){
+	calc.input.value += '9'
+}
+function mumZero(){
+	calc.input.value += '0'
+}
+function mumClear(){
+	calc.input.value = ''
+}
+function mumEquals(){
+	calc.input.value = eval(calc.input.value)
+}
+function mumPlus(){
+	calc.input.value += '+'
+}
+function mumMinus(){
+	calc.input.value += '-'
+}
+function mumDivide(){
+	calc.input.value += '/'
+}
+function mumMultiply(){
+	calc.input.value += '*'
+}
